94e8c17204f08a57609467d54aca9cdcbef26002
Teodoras_Berzanskis <Teodoras.premier@gmail.com>
2020-06-09 22:05:07 +0300

    Index B

--- index.php
+++ index.php
@@ -2,19 +2,7 @@
 <?php include "header.php" ?>
 
   <h1>DGMTV</h1>
-<div class="container">
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">DGMTV</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
-  </div>
+
 
 
 <?php include "footer.php" ?>
