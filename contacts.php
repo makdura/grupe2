174f25145b14af1e66bfd7ec52e95b6b3557d73f
GenadijusG <genadijus.gricukas@gmail.com>
2020-06-09 22:01:26 +0300

    idetas paragrafas

--- contacts.php
+++ contacts.php
@@ -6,5 +6,11 @@
 <!-- Čia kontaktų puslapio vidinė dalis -->
 <h1 class="center-align">Mūsų smagus kolektyvas</h1>
 <img src="images/about.jpg" alt="Bendra nuotrauka">
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et esse dolor nam rerum tenetur similique cupiditate optio reprehenderit saepe, delectus voluptatum quaerat, reiciendis ex. Neque, obcaecati? Non, repellat omnis?
+</p>
+
+
+
 
 <?php include "footer.php" ?>
