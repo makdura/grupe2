56c4149a3ce0da0df61f02d4ab5c2a55a3273dfa
WaldemarZ <valdemar.zilovic@gmail.com>
2020-06-10 19:19:05 +0300

    Portfolio changes

--- gallery.php
+++ gallery.php
@@ -2,8 +2,80 @@
 <?php include "header.php" ?>
 
 
-<img class="pic" src="images/gallery1.jpg" alt="pvz">
+<div class="row center-cols">
+    <div class="col m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/project-1.jpg">
+          <span class="card-title">Pirmas projektas</span>
+        </div>
+        <div class="card-content">
+          <p>Tai yra mūsų pirmas projektas kuris buvo puikus !</p>
+        </div>
+       </div>
+    </div>
+  
+    <div class="col m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/project-2.jpg">
+          <span class="card-title">Antras projektas</span>
+        </div>
+        <div class="card-content">
+          <p>Tai yra mūsų antras projektas kuris buvo geras !</p>
+        </div>
+       </div>
+    </div>
 
+    <div class="col m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/project-3.jpg">
+          <span class="card-title">Trečias projektas</span>
+        </div>
+        <div class="card-content">
+          <p>Tai yra mūsų trečias projektas kuris buvo neblogas !</p>
+        </div>
+       </div>
+    </div>
+
+    <div class="col m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/project-4.jpg">
+          <span class="card-title">Ketvirtas projektas</span>
+        </div>
+        <div class="card-content">
+          <p>Tai yra mūsų ketvirtas projektas kuris buvo geresnis !</p>
+        </div>
+       </div>
+    </div>
+
+    <div class="col m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/project-7.jpg">
+          <span class="card-title">Penktas projektas</span>
+        </div>
+        <div class="card-content">
+          <p>Tai yra mūsų penktas projektas kuris buvo gerulis !</p>
+        </div>
+       </div>
+    </div>
+
+    <div class="col m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/project-6.jpg">
+          <span class="card-title">Šeštas projektas</span>
+        </div>
+        <div class="card-content">
+          <p>Tai yra mūsų šeštas projektas kuris buvo nuostabus !</p>
+        </div>
+       </div>
+    </div>
+</div>
+  
 
 
 <?php include "footer.php" ?>
