7bff403e3d9fece713febb454ef630255467bd9f
makdura <dominykas.poderys@gmail.com>
2020-06-10 19:58:45 +0300

    header_script

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Sidenav.init(elems);
+
+});
