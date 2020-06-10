7f2a7f0e76d66cda711b87612668dc3b0853aefc
MantasKelpsas <satnam32@gmail.com>
2020-06-10 20:34:39 +0300

    pataisytos foto

--- about.php
+++ about.php
@@ -10,27 +10,29 @@
 
     <p class="flow-text center-align">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum eos consectetur, voluptatem fuga perferendis numquam, ea incidunt excepturi necessitatibus, eum quas quisquam tempore. Quo amet illum provident atque similique omnis!</p>
+        
+    <div class="row center-align">
+        <div class="col s12 m6 l3">
+            <img class="responsive-img" src="images/apie1.jpg" alt="nuotrauka1">
+            <p>Viską darom</p>
+        </div>
+        <div class="col s12 m6 l3">
+            <img class="responsive-img" src="images/apie2.jpg" alt="nuotrauka2">
+            <p>Darom ką norim</p>
+        </div>
+        <div class="col s12 m6 l3">
+            <img class="responsive-img" src="images/apie3.jpg" alt="nuotrauka3">
+            <p>Vėl viską darom</p>
+        </div>
+        <div class="col s12 m6 l3">
+            <img class="responsive-img" src="images/apie4.jpg" alt="nuotrauka4">
+            <p>Kažką darom</p>
+        </div>
+
+    </div>
 </div>
 
-<div class="row center-align">
-    <div class="col s12 m6 l3">
-        <img class="responsive-img" src="iamges/apie1.jpg" alt="nuotrauka1">
-        <h6>Nuotraukos tekstas</h6>
-    </div>
-    <div class="col s12 m6 l3">
-        <img class="responsive-img" src="iamges/apie2.jpg" alt="nuotrauka2">
-        <h6>Nuotraukos tekstas</h6>
-    </div>
-    <div class="col s12 m6 l3">
-        <img class="responsive-img" src="iamges/apie3.jpg" alt="nuotrauka3">
-        <h6>Nuotraukos tekstas</h6>
-    </div>
-    <div class="col s12 m6 l3">
-        <img class="responsive-img" src="iamges/apie4.jpg" alt="nuotrauka4">
-        <h6>Nuotraukos tekstas</h6>
-    </div>
 
-</div>
 
 
 
