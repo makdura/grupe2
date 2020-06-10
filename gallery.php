23e59810cf8756a91971392cfa796914015cd948
WaldemarZ <valdemar.zilovic@gmail.com>
2020-06-10 19:47:17 +0300

    Gallery changes2

--- gallery.php
+++ gallery.php
@@ -1,10 +1,28 @@
 
 <?php include "header.php" ?>
 
+<div class="row center-cols">
+<div class="col s12 m8 offset-m4 l6 offset-l3">
+        <div class="card-panel grey lighten-5 z-depth-2">
+          <div class="row valign-wrapper">
+            <div class="col s3">
+              <img src="images/3DGMTV.png" alt="" class="circle responsive-img"> 
+            </div>
+            <div class="col s18">
+              <h6 class="black-text">
+                Čia yra mūsų įmonės projektai. Mes jais labai didžiuojames.
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+
 
 <div class="row center-cols">
     <div class="col m4">
       <div class="card">
+      <div class="card-panel hoverable">
         <div class="card-image">
           <img src="images/project-1.jpg">
           <span class="card-title">Pirmas projektas</span>
@@ -14,9 +32,11 @@
         </div>
        </div>
     </div>
+    </div>
   
     <div class="col m4">
       <div class="card">
+      <div class="card-panel hoverable">
         <div class="card-image">
           <img src="images/project-2.jpg">
           <span class="card-title">Antras projektas</span>
@@ -26,9 +46,11 @@
         </div>
        </div>
     </div>
+    </div>
 
     <div class="col m4">
       <div class="card">
+      <div class="card-panel hoverable">
         <div class="card-image">
           <img src="images/project-3.jpg">
           <span class="card-title">Trečias projektas</span>
@@ -38,9 +60,11 @@
         </div>
        </div>
     </div>
+    </div>
 
     <div class="col m4">
       <div class="card">
+      <div class="card-panel hoverable">
         <div class="card-image">
           <img src="images/project-4.jpg">
           <span class="card-title">Ketvirtas projektas</span>
@@ -50,9 +74,11 @@
         </div>
        </div>
     </div>
+    </div>
 
     <div class="col m4">
       <div class="card">
+      <div class="card-panel hoverable">
         <div class="card-image">
           <img src="images/project-7.jpg">
           <span class="card-title">Penktas projektas</span>
@@ -62,9 +88,11 @@
         </div>
        </div>
     </div>
+    </div>
 
     <div class="col m4">
       <div class="card">
+      <div class="card-panel hoverable">
         <div class="card-image">
           <img src="images/project-6.jpg">
           <span class="card-title">Šeštas projektas</span>
@@ -73,6 +101,7 @@
           <p>Tai yra mūsų šeštas projektas kuris buvo nuostabus !</p>
         </div>
        </div>
+    </div>
     </div>
 </div>
   
